eda0fa897f049f7b39b85d64dba701ae7bcc8a4d
GT2_DeathGun005 <francesco.05marino@gmail.com>
2025-12-01 15:10:27 +0100

    Basic javafx run mockup and modified pom file

--- src/main/java/poco/company/group01pocolib/mvc/PocoLibApplication.java
+++ src/main/java/poco/company/group01pocolib/mvc/PocoLibApplication.java
@@ -2,8 +2,10 @@ package poco.company.group01pocolib.mvc;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.net.URL;
 
 import java.io.IOException;
 
@@ -11,6 +13,10 @@ public class PocoLibApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-
+        URL urlRisorsa = getClass().getResource("/poco/company/group01pocolib/Mockup.fxml");
+        Parent root = FXMLLoader.load(urlRisorsa);
+        stage.setScene(new Scene(root));
+        stage.setTitle("Mockup");
+        stage.show();
     }
 }
