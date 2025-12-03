b642e55d5bc007a1e00271fb7740f557223aa758
GT2_DeathGun005 <francesco.05marino@gmail.com>
2025-12-03 18:22:55 +0100

    Added TestClass

---
+++ src/test/java/poco/company/group01pocolib/TestClass.java
@@ -0,0 +1,13 @@
+package poco.company.group01pocolib;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestClass {
+	
+	@Test
+	public void testExample() {
+		// TODO: Implement test
+		assertTrue(true);
+	}
+}
