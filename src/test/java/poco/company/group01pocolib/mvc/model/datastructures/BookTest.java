95a54c46e1686e7d79c5f689e2717a4e3619ae97
pepo-dev <d.pepe26@studenti.unisa.it>
2025-12-06 18:40:15 +0100

    Add unit tests for BookSet, Book, LendingSet, Lending, UserSet, and User classes

---
+++ src/test/java/poco/company/group01pocolib/mvc/model/datastructures/BookTest.java
@@ -0,0 +1,57 @@
+/**
+ * @file BookTest.java
+ * @brief Unit tests for the Book class.
+ * @author Daniele Pepe
+ * @date 6 December 2025
+ * @version 0.1
+ */
+package poco.company.group01pocolib.mvc.model.datastructures;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @class BookTest
+ * @brief Contains unit tests to verify the correctness of the Book class methods and validation logic.
+ */
+public class BookTest {
+
+    /**
+     * @brief Tests that a Book object is created correctly when valid arguments are provided.
+     */
+    @Test
+    public void testBookCreationValid() {
+        // TODO : implementation
+    }
+
+    /**
+     * @brief Tests that the constructor throws IsbnNotValidException when an invalid ISBN is provided.
+     */
+    @Test
+    public void testBookCreationInvalidIsbn() {
+        // TODO : implementation
+    }
+
+    /**
+     * @brief Tests the addCopy method to ensure copy count increases.
+     */
+    @Test
+    public void testAddCopy() {
+        // TODO : implementation
+    }
+
+    /**
+     * @brief Tests the removeCopy method to ensure copy count decreases when copies are available.
+     */
+    @Test
+    public void testRemoveCopyValid() {
+        // TODO : implementation
+    }
+
+    /**
+     * @brief Tests that removeCopy throws an exception (IllegalStateException) when no copies are available.
+     */
+    @Test
+    public void testRemoveCopyInvalid() {
+        // TODO : implementation
+    }
+}
---
+++ src/test/java/poco/company/group01pocolib/mvc/model/datastructures/UserSetTest.java
@@ -0,0 +1,57 @@
+/**
+ * @file UserSetTest.java
+ * @brief Unit tests for the UserSet class.
+ * @author Daniele Pepe
+ * @date 6 December 2025
+ * @version 0.1
+ */
+package poco.company.group01pocolib.mvc.model.datastructures;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @class UserSetTest
+ * @brief Contains unit tests to verify the correctness of the UserSet class methods.
+ */
+public class UserSetTest {
+
+    /**
+     * @brief Tests that a UserSet object is created correctly.
+     */
+    @Test
+    public void testUserSetCreation() {
+        // TODO : implementation
+    }
+
+    /**
+     * @brief Tests the addUser method to ensure a user is added correctly.
+     */
+    @Test
+    public void testAddUser() {
+        // TODO : implementation
+    }
+
+    /**
+     * @brief Tests the removeUser method to ensure a user is removed correctly.
+     */
+    @Test
+    public void testRemoveUser() {
+        // TODO : implementation
+    }
+
+    /**
+     * @brief Tests the getUserById method to ensure it returns the correct user.
+     */
+    @Test
+    public void testGetUserById() {
+        // TODO : implementation
+    }
+
+    /**
+     * @brief Tests that getUserById returns null when the user is not found.
+     */
+    @Test
+    public void testGetUserByIdNotFound() {
+        // TODO : implementation
+    }
+}
