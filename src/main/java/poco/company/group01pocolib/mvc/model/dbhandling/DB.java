f6a141bd8d5c8b35df4e782cbfe491f77af1f6b5
GT2_DeathGun005 <francesco.05marino@gmail.com>
2025-12-06 15:32:08 +0100

    Added and documented Backup and DB classes

--- src/main/java/poco/company/group01pocolib/mvc/model/dbhandling/DB.java
+++ src/main/java/poco/company/group01pocolib/mvc/model/dbhandling/DB.java
@@ -1,4 +1,38 @@
+/**
+ * @file DB.java
+ * @brief This file contains the DB class responsible for saving and loading the library data
+ * @author Francesco Marino
+ * @date 6 December 2025
+ */
 package poco.company.group01pocolib.mvc.model.dbhandling;
 
+import poco.company.group01pocolib.mvc.model.datastructures.BookSet;
+import poco.company.group01pocolib.mvc.PocoLibModel;
+import poco.company.group01pocolib.mvc.model.datastructures.LendingSet;
+import poco.company.group01pocolib.mvc.model.datastructures.UserSet;
+
+/**
+ * @class DB
+ * @brief Responsible for saving and loading the library data
+ */
 public class DB {
+    private static final String DATA_FILE = "library_data.bin";
+
+    /**
+     * @brief Saves the current state of the library data to a file
+     * @param bookSet The set of books to save
+     * @param userSet The set of users to save
+     * @param lendingSet The set of lendings to save
+     */
+    public static void save(BookSet bookSet, UserSet userSet, LendingSet lendingSet) {
+        //TODO: Implement backup logic
+    }
+
+    /**
+     * @brief Loads the library data from a file
+     * @param model The PocoLibModel instance to load data into
+     */
+    public static void load(PocoLibModel model) {
+        //TODO: Implement load logic
+    }
 }
