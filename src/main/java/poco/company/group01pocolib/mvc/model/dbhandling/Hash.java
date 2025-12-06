c382a26721b92facc2a954b0d22bce9fa3460e2d
Vito Palmieri <v.palmieri11@studenti.unisa.it>
2025-12-06 17:56:35 +0100

    Refactor DB class to enhance database handling and introduce Hash utility for file hashing

---
+++ src/main/java/poco/company/group01pocolib/mvc/model/dbhandling/Hash.java
@@ -0,0 +1,33 @@
+package poco.company.group01pocolib.mvc.model.dbhandling;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
+
+/**
+ * Simple class containing two static methods, one to hash a file, and another to compare two files by their hashes.
+ */
+public class Hash {
+    /**
+     * This method takes a {@code path} as input, and calculates a hash for the corresponding file using SHA-256.
+     * @param path Path to the file we intend to hash
+     * @return String containing the hash of the input file, calculated using SHA-256
+     */
+    static public String getFileHash(Path path) {
+        return "";
+    }
+
+    /**
+     * This method takes 2 files as input (through their paths) and calculates the hashes for both of them. Then, it returns
+     * the result of the comparison
+     * @param p1 Path to the first file
+     * @param p2 Path to the second file
+     * @return {@code true} if the hashes correspond, {@code false} if they don't
+     */
+    static public boolean compareFileHashes(Path p1, Path p2) {
+        return getFileHash(p1).equals(getFileHash(p2));
+    }
+}
