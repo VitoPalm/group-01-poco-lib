9752f2313baef03184b51d40b9641ecd485468ba
Vito Palmieri <v.palmieri11@studenti.unisa.it>
2025-12-13 15:33:14 +0100

    Finally implemented Index class
    Added a fast remove method.
    
    Signed-off-by: Vito Palmieri <v.palmieri11@studenti.unisa.it>

--- src/main/java/poco/company/group01pocolib/db/omnisearch/Index.java
+++ src/main/java/poco/company/group01pocolib/db/omnisearch/Index.java
@@ -2,10 +2,7 @@ package poco.company.group01pocolib.db.omnisearch;
 
 import java.io.Serial;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @class   Index
@@ -20,7 +17,10 @@ public class Index<T> implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    private Map<String, List<T>> triMappings;
+    private Map<String, Set<T>> triMappings;
+
+    // Character used to pad strings shorter than 3 characters after processing
+    private static final String PADDING_CHAR = "\u0007";
 
     /**
      * @brief   Default constructor initializes the trigram mappings.
@@ -33,7 +33,7 @@ public class Index<T> implements Serializable {
      * @brief   Get the trigram mappings.
      * @return  A map of trigrams to lists of items.
      */
-    public Map<String, List<T>> getTriMappings() {
+    public Map<String, Set<T>> getTriMappings() {
         return triMappings;
     }
 
@@ -49,11 +49,30 @@ public class Index<T> implements Serializable {
      *          whitespace.
      *
      * @param   searchableContent The content to generate trigrams from.
-     * @return  A list of generated trigrams.
+     * @return  A `List` of generated trigrams. `null` if the input `String` is empty.
      */
     public List<String> generateTrigrams(String searchableContent) {
-        // TODO: implement -- as ArrayList
-        return null;
+        String processedContent = searchableContent.toLowerCase().trim();
+
+        ArrayList<String> trigrams = new ArrayList<>();
+
+        if (processedContent.isEmpty()) {
+            trigrams = null;
+        } else if (processedContent.length() < 3) {
+            StringBuilder output = new StringBuilder(processedContent);
+
+            while (output.length() < 3) {
+                output.append('\u0007');
+            }
+
+            trigrams.add(output.toString());
+        } else {
+            for (int i = 0; i < processedContent.length() - 2; i++) {
+                trigrams.add(processedContent.substring(i, i+3));
+            }
+        }
+
+        return trigrams;
     }
 
     /**
@@ -63,14 +82,47 @@ public class Index<T> implements Serializable {
      * @param   item The item to be indexed.
      */
     public void add(String searchableContent, T item) {
-        // TODO: implement
+        List<String> keys = generateTrigrams(searchableContent);
+        if (keys == null) return;
+
+        for (String key : keys) {
+            // Only does one lookup on map
+            triMappings.computeIfAbsent(key, k -> new HashSet<>()).add(item);
+        }
     }
 
     /**
      * @brief   Removes an item from the index.
      * @param   item The item to be removed from the index.
      */
-    public void remove (T item) {
-        // TODO: implement
+    public void remove(T item) {
+        // Ngl, refactoring of classic iterator design was suggested by IDE
+        triMappings.values().removeIf(
+                set -> set.remove(item) && set.isEmpty());
+    }
+
+    /**
+     * @brief   Removes an item from the index using searchableContent related to it.
+     *
+     * @param   searchableContent   The `String` associated with `item`.
+     * @param   item                The item to be removed from the index.
+     *
+     * @warning If the `searchableContent` used to remove `item` isn't the same as the original one used to add `item`,
+     *          `item` may not be completely be removed from the index. To make sure `item` is removed, use {@link
+     *          poco.company.group01pocolib.db.omnisearch.Index.remove remove(T item)} instead.
+     */
+    public void fastRemove(String searchableContent, T item) {
+        List<String> keys = generateTrigrams(searchableContent);
+
+        for (String key : keys) {
+            Set<T> items = triMappings.get(key);
+
+            if (items != null) {
+                items.remove(item);
+                if (items.isEmpty()) {
+                    triMappings.remove(key);
+                }
+            }
+        }
     }
 }
