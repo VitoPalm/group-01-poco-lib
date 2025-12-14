371a90a44e58881e6bdc8dc1c0db7ed6292f7d6f
Vito Palmieri <v.palmieri11@studenti.unisa.it>
2025-12-15 00:34:35 +0100

    Refactor Index class to use ngrams instead of trigrams for improved flexibility.
    Signed-off-by: Vito Palmieri <v.palmieri11@studenti.unisa.it>

--- src/main/java/poco/company/group01pocolib/db/omnisearch/Index.java
+++ src/main/java/poco/company/group01pocolib/db/omnisearch/Index.java
@@ -6,10 +6,10 @@ import java.util.*;
 
 /**
  * @class   Index
- * @brief   Represents a trigram index for fast searching of items based on a provided `String`.
- * @details This class allows adding, removing, and generating trigrams for searchable content associated with items of
- *          type `T`. The trigrams are stored in a map where each trigram maps to a list of items related to that
- *          trigram.
+ * @brief   Represents an ngram index for fast searching of items based on a provided `String`.
+ * @details This class allows adding, removing, and generating ngrams for searchable content associated with items of
+ *          type `T`. The ngrams are stored in a map where each ngram maps to a list of items related to that
+ *          ngram.
  *
  * @param   <T> The type of items to be indexed.
  */
@@ -17,77 +17,78 @@ public class Index<T> implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    private Map<String, Set<T>> triMappings;
+    private Map<String, Set<T>> ngramMappings;
 
+    // Ngram size used for indexing
+    public static final int NGRAM_SIZE = 3;
     // Character used to pad strings shorter than 3 characters after processing
-    private static final String PADDING_CHAR = "\u0007";
+    public static final String PADDING_CHAR = "\u0007";
 
     /**
-     * @brief   Default constructor initializes the trigram mappings.
+     * @brief   Default constructor initializes the ngram mappings.
      */
     public Index() {
-        this.triMappings = new HashMap<>();
+        this.ngramMappings = new HashMap<>();
     }
 
     /**
-     * @brief   Get the trigram mappings.
-     * @return  A map of trigrams to lists of items.
+     * @brief   Get the ngram mappings.
+     * @return  A map of ngrams to lists of items.
      */
-    public Map<String, Set<T>> getTriMappings() {
-        return triMappings;
+    public Map<String, Set<T>> getNGramMappings() {
+        return ngramMappings;
     }
 
     /**
-     * @brief   Generates trigrams from the provided searchable content.
-     * @details A trigram is a contiguous sequence of three characters from a given string. This method extracts all
-     *          possible trigrams from the input string and returns them as a list.
+     * @brief   Generates ngrams from the provided searchable content.
+     * @details An ngram is a contiguous sequence of three characters from a given string. This method extracts all
+     *          possible ngrams from the input string and returns them as a list.
      *          <br><br>
-     *          If the input string is shorter than three characters, it will generate all possible combinations of the
-     *          Ngram available, that result into a trigram when adding bell characters as padding.
+     *          If the input string is shorter than `NGRAM_SIZE` characters, it will add padding characters (defined by
+     *          `PADDING_CHAR`) to the end of the string until it reaches a length of `NGRAM_SIZE`.
      *          <br><br>
-     *          The method does some basic preprocessing, such as converting the string to lowercase, and trimming
-     *          whitespace.
+     *          The method trims leading and trailing whitespace before generating ngrams.
      *
-     * @param   searchableContent The content to generate trigrams from.
-     * @return  A `List` of generated trigrams. `null` if the input `String` is empty.
+     * @param   searchableContent The content to generate ngrams from.
+     * @return  A `List` of generated ngrams. `null` if the input `String` is empty.
      */
-    public List<String> generateTrigrams(String searchableContent) {
-        String processedContent = searchableContent.toLowerCase().trim();
+    public List<String> generateNGrams(String searchableContent) {
+        String processedContent = searchableContent.trim();
 
-        ArrayList<String> trigrams = new ArrayList<>();
+        ArrayList<String> ngrams = new ArrayList<>();
 
         if (processedContent.isEmpty()) {
-            trigrams = null;
-        } else if (processedContent.length() < 3) {
+            ngrams = null;
+        } else if (processedContent.length() < NGRAM_SIZE) {
             StringBuilder output = new StringBuilder(processedContent);
 
-            while (output.length() < 3) {
-                output.append('\u0007');
+            while (output.length() < NGRAM_SIZE) {
+                output.append(PADDING_CHAR);
             }
 
-            trigrams.add(output.toString());
+            ngrams.add(output.toString());
         } else {
-            for (int i = 0; i < processedContent.length() - 2; i++) {
-                trigrams.add(processedContent.substring(i, i+3));
+            for (int i = 0; i < processedContent.length() - NGRAM_SIZE + 1; i++) {
+                ngrams.add(processedContent.substring(i, i+NGRAM_SIZE));
             }
         }
 
-        return trigrams;
+        return ngrams;
     }
 
     /**
      * @brief   Adds an item to the index based on the provided searchable content.
      *
-     * @param   searchableContent The content used to generate trigrams for indexing.
+     * @param   searchableContent The content used to generate ngrams for indexing.
      * @param   item The item to be indexed.
      */
     public void add(String searchableContent, T item) {
-        List<String> keys = generateTrigrams(searchableContent);
+        List<String> keys = generateNGrams(searchableContent);
         if (keys == null) return;
 
         for (String key : keys) {
             // Only does one lookup on map
-            triMappings.computeIfAbsent(key, k -> new HashSet<>()).add(item);
+            ngramMappings.computeIfAbsent(key, k -> new HashSet<>()).add(item);
         }
     }
 
@@ -97,7 +98,7 @@ public class Index<T> implements Serializable {
      */
     public void remove(T item) {
         // Ngl, refactoring of classic iterator design was suggested by IDE
-        triMappings.values().removeIf(
+        ngramMappings.values().removeIf(
                 set -> set.remove(item) && set.isEmpty());
     }
 
@@ -112,16 +113,16 @@ public class Index<T> implements Serializable {
      *          poco.company.group01pocolib.db.omnisearch.Index#remove remove(T item)} instead.
      */
     public void fastRemove(String searchableContent, T item) {
-        List<String> keys = generateTrigrams(searchableContent);
+        List<String> keys = generateNGrams(searchableContent);
         if (keys == null) return;
 
         for (String key : keys) {
-            Set<T> items = triMappings.get(key);
+            Set<T> items = ngramMappings.get(key);
 
             if (items != null) {
                 items.remove(item);
                 if (items.isEmpty()) {
-                    triMappings.remove(key);
+                    ngramMappings.remove(key);
                 }
             }
         }
